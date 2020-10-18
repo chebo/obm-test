dca1aef0ec72ffaa3b131ea5f4975632789db734
Grigoriy Chebotarev <g.chebo@gmail.com>
2020-10-19 02:12:34 +0300

    interview test tevelopment

--- automation_behave/features/first_test.feature
+++ automation_behave/features/first_test.feature
@@ -4,3 +4,8 @@ Scenario: Run a simple test
     Given I open http://openbodymind.com/ using chrome
     When I have 6 links within school menu items using chrome
     Then I take screenshot using chrome
+
+Scenario: Check of students examination results
+    Given I open http://openbodymind.com/articles/testing/ using chrome
+    When I have read list of students items using chrome
+    Then I take screenshot using chrome
--- automation_behave/features/steps/web_interface.py
+++ automation_behave/features/steps/web_interface.py
@@ -46,3 +46,28 @@ def step_impl(context, browser):
     """
     client = getattr(context, browser)
     client.save_screenshot("screenshot.png")
+
+
+@when("I have read list of students items using (?P<browser>.+)")
+def step_impl(context, browser):
+    """
+    :type context: behave.runner.Context
+    :type browser: str
+    """
+    count = 0
+    client = getattr(context, browser)
+    students_card = dict()
+    student_list = client.find_element_by_class_name("list")
+    students = [x for x in student_list.find_elements_by_class_name("student")]
+    for x in students:
+        students_card[x.text.split(",")[0]] = x.text.split(",")[1].split("-")
+
+    for x in students_card.keys():
+        student_grade = students_card[x][0].replace(' ', '')
+        student_result = students_card[x][1].replace(' ', '')
+        if float(student_grade) >= 7.5:
+            assert student_result == "Passed", "Result lower, then expected for student {}".format(x)
+        else:
+            assert student_result == "Failed", "Result higher, then expected for student {}".format(x)
+
+    time.sleep(Env.vars['wait_time'])
